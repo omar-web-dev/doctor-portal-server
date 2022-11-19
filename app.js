a60eb22a1ed4f33d169ec6685ec9f866013b9b6e
omarwebdeve <omarf977865@gmail.com>
2022-11-19 22:00:01 +0600

    token assecc

--- app.js
+++ app.js
@@ -90,12 +90,12 @@ async function run(){
 
         app.get('/jwt', async(req,res) => {
             const email = req.query.email
-            const query = {email}
+            const query = {email : email}
             const user = await usersCollection.findOne(query)
             console.log(user)
             if(user){
                 const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn : '23h'})
-                res.send({accessToken : token})
+                return res.send({accessToken : token})
             }
             res.status(403).send({assesToken: ''})
         })
